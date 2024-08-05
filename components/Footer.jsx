51c24a9d71f9bba6278d4d4c17d66888335667b0
DaniTech <tochidan11@gmail.com>
2024-08-05 08:50:07 -0700

    Done with Footer jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Image from "next/image";
+import { RiSendPlaneFill } from "react-icons/ri";
+import {
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialLinkedin,
+  TiSocialInstagram,
+  TiSocialFacebook,
+} from "react-icons/ti";
 
+//INTERNAL IMPORT
+import Style from "../styles/Footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={Style.footer}>
+      <div className={Style}>
+        <Image src={footerLogo} alt="logo" width={100} height={100} />
+      </div>
+
+      <div className={Style.footer_box}>
+        <div className={Style.footer_input}>
+          <input type="email" placeholder="Email" />
+          <RiSendPlaneFill />
+        </div>
+      </div>
+
+      <div className={Style.footer_box}>
+        <div className={Style.social}>
+          <TiSocialFacebook />
+          <TiSocialInstagram />
+          <TiSocialLinkedin />
+          <TiSocialTwitter />
+          <TiSocialYoutube />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
