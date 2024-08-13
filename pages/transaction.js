2af10f45d4fc844d97ba6ca897d8789cf160679d
DaniTechDev <tochidan11@gmail.com>
2024-08-13 05:01:40 -0700

    On Transaction page. Make api call using JsonRpcProvider from context api

--- pages/transaction.js
+++ pages/transaction.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Link from "next/link";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+
+//INTERNALIMPORT
+import StyleTransaction from "../styles/block.module.css";
+import { ETHERSCAN } from "../Context/Ether";
 
 const transaction = () => {
+  const { provider } = useContext(ETHERSCAN);
+  const router = useRouter();
+
+  const { query } = router;
+  const hash = Object.keys(query)[0];
+
+  const transDetails = [];
+  const [transactionData, setTransactionData] = useState(transDetails);
+
+  //FORMAT VALUE
+  const [ethGasLimit, setEthGasLimit] = useState("");
+  const [ethDifficulty, setEthDifficulty] = useState("");
+  const [VALUE, setVALUE] = useState("");
+
+  const getDataOfTransaction = async () => {
+    try {
+      const transactionDetails = await provider.getTransaction(hash);
+      setTransactionData(transactionDetails);
+
+      console.log("transactionDetails", transactionDetails);
+    } catch (error) {
+      console.log("Something is wrong from the transaction page");
+    }
+  };
+
+  useState(() => {
+    getDataOfTransaction();
+  }, []);
+
   return <div>transaction</div>;
 };
 
